520b1afe57c43c334fc117885957649087d9472f
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2020-12-22 18:28:06 +0000

    added giphy search bar but doesn't handle if no gif found

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-let baseUrl = "https://api.giphy.com/v1/gifs/translate?";
-
-let locationSearch = "cats";
-let locationExtension = "s=" + locationSearch;
-
-let APIKey = "7jtL7ys5EbsJngKhvLEuz7QV8UPTfKZY";
-let APIExtension = "&api_key=" + APIKey;
-
-let url = baseUrl + locationExtension + APIExtension;
-
-fetch(url, {
-  mode: "cors",
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
-
+const form = document.querySelector("#search-form");
 const img = document.querySelector("img");
+
+const baseUrl = "https://api.giphy.com/v1/gifs/translate?";
+
+const APIKey = "7jtL7ys5EbsJngKhvLEuz7QV8UPTfKZY";
+const APIExtension = "&api_key=" + APIKey;
+
+const handleGiphy = (searchValue) => {
+  let locationSearch = "cats";
+
+  if (searchValue !== undefined) locationSearch = searchValue;
+  const locationExtension = "s=" + locationSearch;
+
+  let url = baseUrl + locationExtension + APIExtension;
+
+  fetch(url, {
+    mode: "cors",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+};
+
+const handleForm = (e) => {
+  e.preventDefault();
+  const searchValue = e.target.elements["search"].value;
+  handleGiphy(searchValue);
+};
+
+handleGiphy();
+
+form.addEventListener("submit", handleForm);
